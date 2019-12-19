f908930d29fd391b2b0281c643f5ad12c2fd3efb
bkarakas <bkarakasu@gmail.com>
2019-12-18 19:20:20 -0600

    route53 was created

---
+++ route53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "www" { 
+  zone_id = "Z1NSGWEPJFDNW" 
+  name    = "www.burakkarakas.net" 
+  type    = "A" 
+  ttl     = "60" 
+  records = [aws_instance.web.public_ip] 
+} 
