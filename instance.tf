0862e1c2a58764892ca16b42f5950b3c64c07fce
bkarakas <bkarakasu@gmail.com>
2019-12-21 12:30:34 -0600

    instances variables updated

--- instance.tf
+++ instance.tf
@@ -1,31 +1,30 @@
-resource "aws_instance" "web" { 
+resource "aws_instance" "web" {
   count = var.count_instance
-  ami           = var.ami
-  instance_type = var.instance_type
+  ami             = var.ami
+  instance_type   = var.instance_type
   associate_public_ip_address = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
-  security_groups = ["allow_ssh"] 
-  provisioner "remote-exec" { 
-    connection { 
-      host        = self.public_ip 
-      type        = "ssh" 
-      user        = var.user 
-      private_key = file(var.ssh_key_location) 
-    } 
-    inline = [ 
-      "sudo yum install -y epel-release",
-      "sudo yum install httpd -y",
-      "systemctl start httpd", 
-    ] 
-  } 
-  
+  security_groups = ["allow_ssh"]
+  provisioner "remote-exec" {
+    connection {
+      host = self.public_ip
+      type = "ssh"
+      user = var.user
+      private_key = file(var.ssh_key_location)
+      }
+      inline = [
+        "sudo yum install -y epel-release",
+        "sudo yum install httpd -y ",
+        "systemctl start httpd",
+        ]
+      }
   lifecycle{
     prevent_destroy = false
   }
-tags = { 
+  tags = {
     Name = "HelloWorld${count.index +1}"
-  } 
-} 
+  }
+}
 
 
  
