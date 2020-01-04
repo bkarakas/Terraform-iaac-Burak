6c9d202b7ce15b9b51d506efb0c21868a61c86ac
bkarakas <bkarakasu@gmail.com>
2020-01-04 11:18:55 -0600

    policy added

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -11,6 +11,24 @@ resource "aws_iam_group_membership" "developers_team" {
   users = [ 
     aws_iam_user.tim.name 
   ] 
-  group = aws_iam_group.developers.name
-   
+  group = aws_iam_group.developers.name   
+} 
+
+resource "aws_iam_policy" "policy" { 
+  name = "test_policy3" 
+  path = "/" 
+  description = "My test policy" 
+  policy = <<EOF
+{ 
+"Version": "2012-10-17", 
+"Statement": [ 
+{ 
+"Sid": "VisualEditor0", 
+"Effect": "Allow", 
+"Action": "ec2:*", 
+"Resource": "*" 
+} 
+] 
+} 
+EOF 
 } 
