8375d98fa6214059a554b507fc17b693feaa280a
bkarakas <bkarakasu@gmail.com>
2020-01-04 13:39:01 -0600

    added tags

--- Environments/ec2.tf
+++ Environments/ec2.tf
@@ -2,5 +2,7 @@ resource "aws_instance" "instance" {
     ami= var.ami
     instance_type = var.instance_type
     key_name = var.key_name
-    
+    tages = {
+        Name = var.environment
+    }
 }
