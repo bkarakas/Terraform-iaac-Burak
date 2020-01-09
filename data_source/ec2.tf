5f6cb323281e7a1e0bf03ab16b47148e4a30d757
bkarakas <bkarakasu@gmail.com>
2020-01-08 18:41:23 -0600

    spaces corrected

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -5,11 +5,11 @@ provider "aws" {
 
 # Search for ubuntu latest with the owner
 data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners = ["099720109477"]
+    most_recent = true
+    owners = ["099720109477"]
 }
 
 # Show the AMI id
 output "ami" {
-  value = "${data.aws_ami.ubuntu.id}"
+    value = "${data.aws_ami.ubuntu.id}"
 }
