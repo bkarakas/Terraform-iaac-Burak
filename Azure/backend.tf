67d53bca63bf4fb376240d58cd9905b84f6be80d
bkarakas <bkarakasu@gmail.com>
2020-01-18 13:01:50 -0600

    accesskey added

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -4,7 +4,7 @@ terraform {
     storage_account_name = "example1burak"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
-    subscription_id  = "75a2be04-05bd-40ca-8aba-89b102a591d2"
-    tenant_id        = "39582bc6-811b-4fe2-b695-655280892900"
+    access_key           = "SC8cPM8Rf93KXhIBcy3WOZwv9qDTiR6qq5fEb7r2Z2tr127EFvjaxdoe2ca9JNq7vB+7uijSOJzkBbNb0+KbGg==" 
   }
 }
+
