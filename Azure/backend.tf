d361957bb05654f945518d8925ab6acb106e1ab8
bkarakas <bkarakasu@gmail.com>
2020-01-18 12:55:46 -0600

    subscriptionand tenantid added

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -4,5 +4,7 @@ terraform {
     storage_account_name = "example1burak"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
+    subscription_id  = "75a2be04-05bd-40ca-8aba-89b102a591d2"
+    tenant_id        = "39582bc6-811b-4fe2-b695-655280892900"
   }
 }
